0aa1a7eb2263f92554edcf71b26d62ba9f165a18
AhaD <abuahad35188@gmail.com>
2023-05-28 13:58:23 +0600

    delete api create

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 // mongo db st
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ejfmzqt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -48,7 +48,13 @@ async function run() {
                res.send(result)
           }
      })
-
+     // delate api
+     app.delete('/carts/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) };
+          const result = await cartCollection.deleteOne(query);
+          res.send(result);
+        })
 
      app.post('/carts', async (req,res)=>{
           const item = req.body;
