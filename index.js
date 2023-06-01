fab0ee045cd2932904b4429411b368ecc52c2a24
AhaD <abuahad35188@gmail.com>
2023-06-01 11:42:20 +0600

    post menu iten ande veryfyJWT ande veryfyadmin pass

--- index.js
+++ index.js
@@ -127,6 +127,13 @@ async function run() {
           const result = await menuCollection.find().toArray()
           res.send(result)
      })
+     app.post('/menu', verifyJWT, verifyAdmin, async (req, res) => {
+          const newItem = req.body;
+          const result = await menuCollection.insertOne(newItem)
+          res.send(result);
+        })
+
+        
      // review api
      app.get('/reviews', async(req,res)=>{
           const result = await reviewCollection.find().toArray()
