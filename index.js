4160007b3de13788bf1abd67853cdd1ddc9e02f0
AhaD <abuahad35188@gmail.com>
2023-06-10 16:37:15 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+//     await client.connect();
      const usersCollection = client.db('bistroDb').collection('users')
      const menuCollection = client.db('bistroDb').collection('menu')
      const reviewCollection = client.db('bistroDb').collection('reviews')
